6d0e70fbccdd5919f61b2c8a2dd4d9e51e47a9d5
Ryan McGowan <vitomaserati@gmail.com>
2022-12-03 11:24:54 -0800

    completed

--- 1.py
+++ 1.py
@@ -1,1 +1,5 @@
 # Write your solution for algorithm 1 below
+#Write an algorithm that takes in an unsorted numerical list as an argument and sorts the list (use the sort() method).
+list=[10,8,4,9]
+list.sort()
+print(list)
--- 3.py
+++ 3.py
@@ -1,1 +1,5 @@
 # Write your solution for algorithm 3 below
+#Write an algorithm that takes in an unsorted numerical list as an argument and creates a new sorted list in descending order (use the sorted() function).
+unsorted_list=[10,2,5,1]
+sorted_list= sorted(unsorted_list,reverse= True)
+print(sorted_list)
